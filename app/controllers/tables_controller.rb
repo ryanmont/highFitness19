958964ad1add9e68e2f96a55c52f2619d2b55bd2
Ryan Mont <ryanmont@gmail.com>
2017-07-14 17:48:55 -0600

    changed controller before action to delete instead of destroy

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,5 +1,5 @@
 class TablesController < ApplicationController
-  before_action :set_table, only: [:show, :edit, :update, :destroy]
+  before_action :set_table, only: [:show, :edit, :update, :delete]
 
   # GET /tables
   # GET /tables.json
