d623535be083386d05bb8eb03fa28fd4fa0d3648
Ryan Mont <ryanmont@gmail.com>
2017-07-27 17:58:29 -0600

    added two different lines of code in the initializer assets file to see if they work- the last one did not work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
- Rails.application.config.assets.precompile += %w( *.js *.scss *.coffee )
+Rails.application.config.assets.precompile += %w( application.scss )
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
