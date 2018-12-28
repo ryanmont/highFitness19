001e3ce52f6321bf17c8e32ce0dfa39fa4c2471a
Ryan Mont <ryanmont@gmail.com>
2018-12-27 19:06:19 -0700

    initial

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Dentist
+module Dentistdirectemerald
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
