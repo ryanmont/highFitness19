81536db49c13bf043ec348132113798deea5c27e
Ryan Mont <ryanmont@gmail.com>
2019-01-11 16:10:15 -0700

    precompile assets for heroku

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
 
-  http_basic_authenticate_with name: "admin", password: "Kristen15", except: [:congratulations]
+  http_basic_authenticate_with name: "admin", password: "Kristen15", except: [:congratulations, :faq]
   def atv
   end
 
