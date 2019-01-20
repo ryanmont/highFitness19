8653f6143e418494b5331431a1afdb63ad83db63
Ryan Mont <ryanmont@gmail.com>
2019-01-19 21:25:54 -0600

    added more content to the faq page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
 
   http_basic_authenticate_with name: "admin", password: "Kristen15", except: [:congratulations, :faq]
-  def atv
+
+  def faq
   end
 
 
   def congratulations
   end
+
+
 end
